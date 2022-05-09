a55bcc07b840c4c653b8325c4a7f7e884c950bce
thiagohrcosta <thiagohrcosta86@gmail.com>
2022-05-09 15:22:55 -0300

    Adding button

--- App.tsx
+++ App.tsx
@@ -1,9 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View } from 'react-native';
+
+import AppLoading  from 'expo-app-loading';
+
+import { useFonts, Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter';
+
 import { Widget } from './src/componetns/Widget';
 import { theme } from './src/theme';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />
+  }
+
   return (
     <View style={{
       flex: 1,
