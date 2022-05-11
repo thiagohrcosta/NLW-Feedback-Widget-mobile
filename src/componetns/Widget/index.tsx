ba0fc7757f5e0746ddc666da70c964c00b723bf0
Thiago Costa <thiagohrcosta86@gmail.com>
2022-05-11 00:26:26 -0300

    Form component completed

--- src/componetns/Widget/index.tsx
+++ src/componetns/Widget/index.tsx
@@ -6,10 +6,13 @@ import BottomSheet from '@gorhom/bottom-sheet';
 
 import { gestureHandlerRootHOC } from 'react-native-gesture-handler'
 import { Options } from '../Options';
+import { Form } from '../Form';
 
 import { styles } from './styles';
 import { theme } from '../../theme';
+import { feedbackTypes } from '../../utils/feedbackTypes';
 
+export type FeedbackTypes = keyof typeof feedbackTypes;
 
 function Widget() {
 
@@ -38,7 +41,9 @@ function Widget() {
         backgroundStyle={styles.modal}
         handleIndicatorStyle={styles.indicator}
       >
-        <Options />
+        <Form
+          feedbackType='BUG'
+        />
       </BottomSheet>
     </>
   );
