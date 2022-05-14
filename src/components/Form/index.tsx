08b860910df6a81bc85d72d9939ccc28dcd3ce35
Thiago Costa <thiagohrcosta86@gmail.com>
2022-05-13 23:55:38 -0300

    Mobile version completed

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -9,6 +9,8 @@ import {
   TouchableOpacity
 } from "react-native";
 
+import * as FileSystem from 'expo-file-system';
+
 import { ArrowLeft } from "phosphor-react-native";
 import { captureScreen } from 'react-native-view-shot';
 
@@ -54,10 +56,13 @@ export function Form({ feedbackType, onFeedbackCanceled, onFeedbackSent }: Props
 
     setIsSendingFeedback(true);
 
+    const screenshotBase64 = screenshot && 
+    await FileSystem.readAsStringAsync(screenshot, { encoding: 'base64' });
+
     try {
       await api.post('/feedbacks', {
         type: feedbackType,
-        screenshot,
+        screenshot: `data:image/png;base64, ${screenshotBase64}`,
         comment
       });
 
